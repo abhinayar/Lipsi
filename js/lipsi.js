0900ba1e64bcf4a082a5612bcf4823ba7b585395
Abhi Nayar <abhi_nayar@Abhis-MacBook-Air.local>
2016-02-02 23:36:44 -0500

    First commit: First version bare bones site

---
+++ js/lipsi.js
@@ -0,0 +1,32 @@
+//JS code for Chops Snacks LLC
+//by Abhi Nayar
+
+$.noConflict();
+
+// JavaScript Document
+// Author Name: Saptarang
+// Author URI: http://www.saptarang.org
+// Themeforest: http://themeforest.net/user/saptarang?ref=saptarang
+//  Creation Date: 5 March, 2015
+
+jQuery(document).ready(function($) {
+"use strict";
+    
+    /*
+    //Conforms form width to placeholder width
+    $("input[placeholder]").each(function () {
+        $(this).attr('size', $(this).attr('placeholder').length);
+    });    
+   */
+	/*
+	//Preloader//
+	$(window).load(function() {
+        $('#loader-gif').show(0).delay(1500).hide(0);
+        $('#preloader').show(100).delay(1000).fadeOut(1000);
+	})
+	
+	// WOW - animated content
+	new WOW().init();
+	*/
+  
+});
